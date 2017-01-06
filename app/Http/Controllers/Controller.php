37c1fc6f58b72ae339506245ca0dda461743f0d9
wen.du <wen.du@yingzt.com>
2017-01-06 23:01:24 +0800

    merge update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
 use App\Http\Requests;
 
 class Controller extends BaseController
