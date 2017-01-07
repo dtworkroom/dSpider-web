d0e854adcbd91f9bc334b09580a2b7d5a4cd2794
wen.du <wen.du@yingzt.com>
2017-01-07 17:51:01 +0800

    complete doc

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -88,8 +88,8 @@
 
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        <li><a href="{{ url('/login') }}">登录</a></li>
+                        <li><a href="{{ url('/register') }}">注册</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
