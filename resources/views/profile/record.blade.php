8b02d58808c3590807a4dd638cc328e3a5231060
wen.du <wen.du@yingzt.com>
2017-03-30 13:14:46 +0800

    更新内容

--- resources/views/profile/record.blade.php
+++ resources/views/profile/record.blade.php
@@ -173,7 +173,8 @@
                            <span>错误信息:</span>
                            <pre><code><?php
                                     $json = json_decode($record->msg);
-                                    $a=['\\r\\n'=>"\r\n\t\t",'\\n'=>"\n"];
+                                       //'\\n'=>"\n\t\t"
+                                    $a=['\\'=>"\\",];
                                    echo  htmlentities(strtr(json_encode($json, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE),$a));
                                    ?></code></pre>
                         </div>
